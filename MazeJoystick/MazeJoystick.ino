cbaef15cfb807200eddfc6b6287bd298d176dd3f
Travis <tstrat@email.arizona.edu>
2014-05-12 15:47:36 -0700

    Arduino fixes

--- MazeJoystick/MazeJoystick.ino
+++ MazeJoystick/MazeJoystick.ino
@@ -17,7 +17,10 @@
  int joyPin2 = 1;                 // slider variable connecetd to analog pin 1
  int value1 = 0;                  // variable to read the value from the analog pin 0
  int value2 = 0;                  // variable to read the value from the analog pin 1
-
+ 
+ int val1 = 4; // default direction for joystick
+ int val2 = 4;
+ boolean canMove = true;
  void setup() {  
    // initializes digital pins 0 to 7 as outputs
   Serial.begin(9600);
@@ -33,17 +36,35 @@
   value1 = analogRead(joyPin1);   
   // this small pause is needed between reading
   // analog pins, otherwise we get the same value twice
- delay(100);             
+ //delay(100);             
   // reads the value of the variable resistor 
   value2 = analogRead(joyPin2);   
       
   //delay(value1);
  // delay(value2);
-  
-  if (treatValue(value1) != 52 && treatValue(value2) != 52){
-    Serial.print("x: ");
-    Serial.print(treatValue(value1) - 48);
-    Serial.print("y: ");
-    Serial.println(treatValue(value2) -48);
+ if (!canMove && treatValue(value1) - 48 == 4 && treatValue(value2) - 48 == 4){
+  canMove = true; 
+ }
+  if (canMove){
+    if (treatValue(value1) - 48 == 8){
+      Serial.println(2); // up
+      canMove = false;
+    }
+    if (treatValue(value1) - 48 == 0){
+      Serial.println(0); // down
+      canMove = false;
+    }
+    if (treatValue(value2) - 48 == 0){
+      Serial.println(3); // right
+      canMove = false;
+    }
+    if (treatValue(value2) - 48 == 8){
+      Serial.println(1); // left
+      canMove = false;
+    }
   }
+    //while(!(treatValue(value1) - 48 == 4 && treatValue(value2) - 48 ==4)){
+    //  value1 = analogRead(joyPin1);
+    //  value2 = analogRead(joyPin2);
+    //}
  }
